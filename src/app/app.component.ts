7361db7aa21bfe9db16cf03559401d18571600d2
samucastelloo <samucastelloo@gmail.com>
2026-05-26 17:08:26 +0200

    Implement lazy loaded routing and app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/components/header/header.component';
+import { FooterComponent } from './layout/components/footer/footer.component';
+import { ResourcesComponent } from './features/resources/pages/resources/resources.component';
+import { MapComponent } from './features/map/pages/map/map.component';
+import { CommunityComponent } from './features/community/pages/community/community.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,ResourcesComponent,MapComponent,CommunityComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,38 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './features/home/pages/home/home.component';
 
 export const routes: Routes = [
 
   {
     path: '',
-    component: HomeComponent
+    loadComponent: ()=>
+      import('./features/home/pages/home/home.component').then(
+        (m) => m.HomeComponent
+      )
+    },
+  {
+    path: 'resources',
+    loadComponent: () =>
+      import('./features/resources/pages/resources/resources.component').then(
+        (m) => m.ResourcesComponent
+      )
+  },
+  {
+    path: 'map',
+    loadComponent: () =>
+      import('./features/map/pages/map/map.component').then(
+        (m) => m.MapComponent
+      )
+  },
+  {
+    path: 'community',
+    loadComponent: () =>
+      import('./features/community/pages/community/community.component').then(
+        (m) => m.CommunityComponent
+      )
+  },
+  {
+    path: '**',
+    redirectTo: ''
   }
+
 ];
