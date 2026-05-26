eb5260bb18e91c4ec4b245059441a7a2145f8677
samucastelloo <samucastelloo@gmail.com>
2026-05-26 17:21:58 +0200

    Refactor app to use public layout architecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './layout/components/header/header.component';
-import { FooterComponent } from './layout/components/footer/footer.component';
-import { ResourcesComponent } from './features/resources/pages/resources/resources.component';
-import { MapComponent } from './features/map/pages/map/map.component';
-import { CommunityComponent } from './features/community/pages/community/community.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent,ResourcesComponent,MapComponent,CommunityComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,43 @@
 import { Routes } from '@angular/router';
+import { PublicLayoutComponent } from './layout/pages/public-layout/public-layout.component';
 
 export const routes: Routes = [
-
   {
     path: '',
-    loadComponent: ()=>
-      import('./features/home/pages/home/home.component').then(
-        (m) => m.HomeComponent
-      )
-    },
-  {
-    path: 'resources',
-    loadComponent: () =>
-      import('./features/resources/pages/resources/resources.component').then(
-        (m) => m.ResourcesComponent
-      )
-  },
-  {
-    path: 'map',
-    loadComponent: () =>
-      import('./features/map/pages/map/map.component').then(
-        (m) => m.MapComponent
-      )
-  },
-  {
-    path: 'community',
-    loadComponent: () =>
-      import('./features/community/pages/community/community.component').then(
-        (m) => m.CommunityComponent
-      )
+    component: PublicLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/home/pages/home/home.component').then(
+            (m) => m.HomeComponent
+          )
+      },
+      {
+        path: 'resources',
+        loadComponent: () =>
+          import('./features/resources/pages/resources/resources.component').then(
+            (m) => m.ResourcesComponent
+          )
+      },
+      {
+        path: 'map',
+        loadComponent: () =>
+          import('./features/map/pages/map/map.component').then(
+            (m) => m.MapComponent
+          )
+      },
+      {
+        path: 'community',
+        loadComponent: () =>
+          import('./features/community/pages/community/community.component').then(
+            (m) => m.CommunityComponent
+          )
+      }
+    ]
   },
   {
     path: '**',
     redirectTo: ''
   }
-
 ];
